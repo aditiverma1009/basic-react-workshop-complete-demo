0dc5da81ee747bf54356559f392e27b1bbd07105
aditiverma1009 <aditi_verma@mckinsey.com>
2019-11-25 18:13:36 +0530

    use create react app to  setup app alter directory structure added three components in app component

---
+++ src/Components/App/App.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+    <div className="App">
+      <div className="header">Blog Name</div>
+      <div className="body">Body</div>
+      <div className="footer">Footer</div>
+    </div>
+    );
+  }
+}
+
+export default App;

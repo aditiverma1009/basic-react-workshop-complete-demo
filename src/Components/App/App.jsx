975cd89ebd61d129b98c46b2275554d4fea968df
aditiverma1009 <aditi_verma@mckinsey.com>
2019-12-06 11:42:11 +0530

    add form to add blog, remove redefined blogs

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -12,67 +12,33 @@ class App extends Component {
     };
   }
 
-  componentDidMount = () => {
-    fetch('https://content.guardianapis.com/search?api-key=616fd154-e75f-479b-82c8-80e78ad4b6be')
-      .then((response) => response.json())
-      .then((data) => (data.response.results))
-      .then((results) => {
-        this.setState({
-          allBlogs: results,
-        });
-      });
+  onFormSubmit = (event, content) => {
+    const { title, desc, imageurl } = content;
+    const { allBlogs } = this.state;
+    event.preventDefault();
+    this.setState({
+      allBlogs: [
+        ...allBlogs,
+        {
+          title,
+          imageUrl: imageurl,
+          imageAlt: title,
+          content: desc,
+        },
+      ],
+    });
   };
 
   render() {
     const { allBlogs } = this.state;
-    const formattedBlog = allBlogs.map((blog) => ({
-      title: blog.webTitle,
-      desc: blog.pillarName,
-      date: blog.webPublicationDate,
-      imageUrl: blogImage,
-      imageAtl: blog.sectionName,
-      introText: blog.type,
-      content: blog.webTitle,
-    }));
-    // const allBlogs = [{
-    //   title: 'Title Heading',
-    //   desc: 'Description',
-    //   date: new Date('10-09-1996').getFullYear(),
-    //   imageUrl: blogImage,
-    //   imageAlt: 'Blog Title',
-    //   introText: 'Blog Introduction',
-    //   content: 'Content of the blog',
-    // }, {
-    //   title: 'Title Heading',
-    //   desc: 'Description',
-    //   date: new Date('10-09-1990').getFullYear(),
-    //   imageUrl: blogImage,
-    //   imageAlt: 'Blog Title',
-    //   introText: 'Blog Introduction',
-    //   content: 'Content of the blog',
-    // }, {
-    //   title: 'Title Heading',
-    //   desc: 'Description',
-    //   date: new Date('10-09-1996').getFullYear(),
-    //   imageUrl: blogImage,
-    //   imageAlt: 'Blog Title',
-    //   introText: 'Blog Introduction',
-    //   content: 'Content of the blog',
-    // }, {
-    //   title: 'Title Heading',
-    //   desc: 'Description',
-    //   date: new Date('10-09-1990').getFullYear(),
-    //   imageUrl: blogImage,
-    //   imageAlt: 'Blog Title',
-    //   introText: 'Blog Introduction',
-    //   content: 'Content of the blog',
-    // }];
-
-    console.log(formattedBlog);
-    const blogList = formattedBlog.map((eachBlog) => (<BlogCard details={eachBlog} />));
+    const blogList = allBlogs.map((eachBlog) => (<BlogCard details={eachBlog} />));
     return (
       <div className="app">
-        <HomePage blogList={blogList} blogImage={blogImage} />
+        <HomePage
+          onFormSubmit={(event, content) => this.onFormSubmit(event, content)}
+          blogList={blogList}
+          blogImage={blogImage}
+        />
       </div>
     );
   }
--- src/Components/BlogCard/BlogCard.jsx
+++ src/Components/BlogCard/BlogCard.jsx
@@ -4,16 +4,13 @@ import './BlogCard.scss';
 
 function BlogCard({ details }) {
   const {
-    title, desc, date, imageAlt,
-    imageUrl, introText, content,
+    title, imageAlt,
+    imageUrl, content,
   } = details;
   return (
     <div className="blogCard">
       <div>{title}</div>
-      <div>{`${desc}`}</div>
-      <div>{`${date}`}</div>
       <img src={imageUrl} alt={imageAlt} />
-      <div>{introText}</div>
       <div>{content}</div>
     </div>
   );
@@ -22,11 +19,8 @@ function BlogCard({ details }) {
 BlogCard.propTypes = {
   details: PropTypes.shape({
     title: PropTypes.string,
-    desc: PropTypes.string,
-    date: PropTypes.string,
     imageAlt: PropTypes.string,
     imageUrl: PropTypes.string,
-    introText: PropTypes.string,
     content: PropTypes.string,
   }),
 };
@@ -34,11 +28,8 @@ BlogCard.propTypes = {
 BlogCard.defaultProps = {
   details: PropTypes.shape({
     title: '',
-    desc: '',
-    date: '',
     imageAlt: '',
     imageUrl: '',
-    introText: '',
     content: '',
   }),
 };
